c860b4ff3ac65499b4bfe2d20499180411090e26
Dmytro Nechai <nechaido@gmail.com>
2019-10-20 02:49:59 +0300

    feat: add MVP

---
+++ build/precache-manifest.9282c9bcc22e2593b60fa668a20961c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c22934e83b2c25557fab91b08d3b45f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e737696cc6de30f40c8",
+    "url": "/static/css/main.4edd3476.chunk.css"
+  },
+  {
+    "revision": "0b31349bc9e71fe5952b",
+    "url": "/static/js/2.f0885d35.chunk.js"
+  },
+  {
+    "revision": "0e737696cc6de30f40c8",
+    "url": "/static/js/main.e96d323b.chunk.js"
+  },
+  {
+    "revision": "45b5b40ac3d4c61670e9",
+    "url": "/static/js/runtime-main.a9f79ec1.js"
+  }
+]);
